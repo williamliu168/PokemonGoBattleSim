fe4c69a5b2fc30942dc12f1211b8687d6e3f3c31
tinaliutomato <xihongtina.liu@gmail.com>
2016-09-30 23:05:13 -0400

    header fix

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=Edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta charset="UTF-8">
-  <title>Save, share and collaborate | Blocbox</title>
+  <title>Never lose a battle | PokemonGoBattleSim</title>
  
   <!-- Google Fonts -->
   <link href='http://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic|Varela+Round' rel='stylesheet' type='text/css'>
